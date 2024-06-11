5a8c5cd52685d921aff3ecbba5e42536b8ec2678
MRajczyk <mikolajrajczyk01@gmail.com>
2024-06-11 22:26:06 +0200

    ig works but no error correction yet implemented

--- lab2/analizator_skladniowy.py
+++ lab2/analizator_skladniowy.py
@@ -4,31 +4,98 @@ class InvalidSymbolException(Exception):
     def __str__(self):
         return self.message
 
+first_S = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(']
+first_W = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(']
+first_O = ['+', '-', '*', '/', '^']
+first_C = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    
+curr_char_index = 0
+
 def parse_S(input_str):
-    if input_str[0] == '(':
-        parse_W()
-        return
+    global curr_char_index
+    try:
+        if input_str[curr_char_index] in first_S:
+            if input_str[curr_char_index] == '(':
+                curr_char_index += 1
+                res = parse_W(input_str)
+                if not res or input_str[curr_char_index] != ')':
+                    raise InvalidSymbolException(f"Error at index {curr_char_index}, parser expected symbol ')', but encountered: {input_str[curr_char_index]}")
+                curr_char_index += 1
+            else:
+                res = parse_C(input_str)
+                if res is False:
+                    raise InvalidSymbolException(f"Error at index {curr_char_index}, parser expected one of symbols {first_C}, but encountered: {input_str[curr_char_index]}")
+                while res:
+                    curr_char_index += 1
+                    res = parse_C(input_str)
+                if input_str[curr_char_index] == '.':
+                    curr_char_index += 1
+                    res = parse_C(input_str)
+                    if res is False:
+                        raise InvalidSymbolException(f"Error at index {curr_char_index}, parser expected one of symbols {first_C}, but encountered: {input_str[curr_char_index]}")
+                    while res:
+                        curr_char_index += 1
+                        res = parse_C(input_str)         
+            res = parse_O(input_str)
+            if res == False:
+                if input_str[curr_char_index] == ';':
+                    curr_char_index += 1
+                    # tu mozna ogarnac poprawe jakichs bledow typu spacja ig
+                    if len(input_str) == curr_char_index:
+                        return True
+                    else:
+                        return parse_S(input_str)
+            curr_char_index += 1
+            return parse_S(input_str)
+        else:
+            return False
+    except IndexError:
+        raise InvalidSymbolException(f"Error at index {curr_char_index}, unexpected string end")
+
+
+def parse_W(input_str):
+    global curr_char_index
+    if input_str[curr_char_index] in first_W:
+        if input_str[curr_char_index] == '(':
+            curr_char_index += 1
+            res = parse_W(input_str)
+            if not res or input_str[curr_char_index] != ')':
+                raise InvalidSymbolException(f"Error at index {curr_char_index}, parser expected symbol ')', but encountered: {input_str[curr_char_index]}")
+            curr_char_index += 1
+        else:
+            res = parse_C(input_str)
+            if res is False:
+                raise InvalidSymbolException(f"Error at index {curr_char_index}, parser expected one of symbols {first_C}, but encountered: {input_str[curr_char_index]}")
+            while res:
+                curr_char_index += 1
+                res = parse_C(input_str)
+            if input_str[curr_char_index] == '.':
+                curr_char_index += 1
+                res = parse_C(input_str)
+                if res is False:
+                    raise InvalidSymbolException(f"Error at index {curr_char_index}, parser expected one of symbols {first_C}, but encountered: {input_str[curr_char_index]}")
+                while res:
+                    curr_char_index += 1
+                    res = parse_C(input_str)         
+        res = parse_O(input_str)
+        if res == False:
+            return True
+        else:
+            curr_char_index += 1
+            return parse_W(input_str)
     else:
-        index = 0
-        res = parse_C(input_str[index:])
-        if res is False:
-            raise InvalidSymbolException(f"Błąd na pozycji {index}, parser oczekiwal jednego z symboli [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], a jest: {input_str[index]}")
-        while res:
-            index += 1
-            res = parse_C(input_str[index:])
-        if input_str[index] == '.':
-            # go onto looping for decimal part
-            return
+        return False
 
-
-def parse_W(input_char):
-    return True
-
-def parse_O(input_char):
+def parse_O(input_str):
+    global curr_char_index
+    if input_str[curr_char_index] in first_O:
+        return True
+    return False
     return
 
-def parse_C(input_char):
-    if input_char in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+def parse_C(input_str):
+    global curr_char_index
+    if input_str[curr_char_index] in first_C:
         return True
     return False
 
@@ -39,7 +106,8 @@ if len(input_string) < 1:
     exit(1)
 
 try:
-    parse_S(input_string)
+    if parse_S(input_string) == True:
+        print("Entered strings belongs to grammar")
 except Exception as e:
     print(str(e))
 
